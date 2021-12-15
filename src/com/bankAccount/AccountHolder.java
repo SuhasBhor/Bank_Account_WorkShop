b633e33f521f7a61d7eff9e814c3cee14d57ab7a
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-12-15 22:44:17 +0530

    Calculate Interest Of Savings Account Balance

--- src/com/bankAccount/AccountHolder.java
+++ src/com/bankAccount/AccountHolder.java
@@ -4,26 +4,26 @@ public class AccountHolder {
 	public static void main(String[] args) {
 
 		// Creating two object of class Savings Account
-		SavingsAccount sever1 = new SavingsAccount(2000);
-		SavingsAccount sever2 = new SavingsAccount(3000);
+		SavingsAccount saver1 = new SavingsAccount(2000);
+		SavingsAccount saver2 = new SavingsAccount(3000);
 
-		// Calculating Saving balance of sever1 and sever2 on 0.04 interest
+		// Calculating Saving balance of saver1 and saver2 on 0.04 interest
 
 		SavingsAccount.setAnnualInterestRate(0.04);
-		sever1.calculateMonthlyInterest(); // Calculate saving balance for sever 1
-		System.out.println("Sever1 Balance Is: " + sever1.getSavingBalance());
-		sever2.calculateMonthlyInterest(); // Calculate saving balance for sever 2
-		System.out.println("Sever2 Balance Is: " + sever2.getSavingBalance());
+		saver1.calculateMonthlyInterest(); // Calculate saving balance for saver 1
+		System.out.println("Saver1 Balance Is: " + saver1.getSavingBalance());
+		saver2.calculateMonthlyInterest(); // Calculate saving balance for saver 2
+		System.out.println("Saver2 Balance Is: " + saver2.getSavingBalance());
 
 		System.out.println("-------------------------------------------");
 
-		// Calculating Saving balance of sever1 and sever2 on 0.05 interest
+		// Calculating Saving balance of saver1 and saver2 on 0.05 interest
 
 		SavingsAccount.modifyInterestRate(0.05);
-		sever1.calculateMonthlyInterest(); // Calculate saving balance for sever 1
-		System.out.println("Sever1 Balance Is: " + serer1.getSavingBalance());
-		sever2.calculateMonthlyInterest(); // Calculate saving balance for sever 2
-		System.out.println("Sever2 Balance Is: " + sever2.getSavingBalance());
+		saver1.calculateMonthlyInterest(); // Calculate saving balance for saver 1
+		System.out.println("Saver1 Balance Is: " + serer1.getSavingBalance());
+		saver2.calculateMonthlyInterest(); // Calculate saving balance for saver 2
+		System.out.println("Saver2 Balance Is: " + saver2.getSavingBalance());
 
 	}
 }
